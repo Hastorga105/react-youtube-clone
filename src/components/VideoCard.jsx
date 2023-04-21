0f0ef8691f5ba845788b3e990085cbdfca9d5d25
Hector Astorga <hectorastorga105@hotmail.com>
2023-04-20 21:58:24 -0600

    finished the tutorial

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -7,11 +7,11 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoCha
 const VideoCard = ({ video: { id: {videoId }, snippet } }) => {//obtains videoId from video.id and snippet
     //console.log(video)
   return (
-    <Card className='' sx={{ width: { md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: 0 }}>
+    <Card className='' sx={{ width: { xs: '100%', sm: '358px', md: '320px' }, boxShadow: 'none', borderRadius: 0 }}>
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
             <CardMedia image= { snippet?.thumbnails?.high?.url /*/? for default undefined*/ }
             alt={snippet?.title}
-            sx={{ width: 358, height: 180 }}
+            sx={{ width: { xs: '100%' , sm: '358px', md: '320px' }, height: 180 }}
             />
         </Link>
         <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
